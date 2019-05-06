971280192e92d96315430a0fff2c2f1c8ddeaa11
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-05-06 23:32:58 +0300

    Refactor app name

--- hipo_meal/recipes/apps.py
+++ hipo_meal/recipes/apps.py
@@ -1,5 +1,4 @@
 from django.apps import AppConfig
 
-
-class RecipeConfig(AppConfig):
-    name = 'recipe'
+class RecipesConfig(AppConfig):
+    name = 'recipes'
--- hipo_meal/recipes/urls.py
+++ hipo_meal/recipes/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='recipe-home'),
+    path('', views.home, name='recipes-home'),
 ]
