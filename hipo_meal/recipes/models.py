7a785f95f7bcf2209dfbc0c67635388c0c0e6fd5
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-05-07 01:37:25 +0300

    Create recipe model with ORM

--- hipo_meal/recipes/models.py
+++ hipo_meal/recipes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=75)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    vote_points = models.IntegerField()
+    vote_count = models.IntegerField()
+    like_count = models.IntegerField()
+    # Maybe add an image field here.
