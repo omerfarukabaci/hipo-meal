4cd1d09cc1e116dcf08a0de29527d12099395f57
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-05-07 02:20:32 +0300

    Change timezone for date_posted from UTC to localtime

--- hipo_meal/recipes/models.py
+++ hipo_meal/recipes/models.py
@@ -5,9 +5,9 @@ from django.contrib.auth.models import User
 class Recipe(models.Model):
     title = models.CharField(max_length=75)
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(default=timezone.localtime)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    vote_points = models.IntegerField()
-    vote_count = models.IntegerField()
-    like_count = models.IntegerField()
+    vote_points = models.IntegerField(default=0)
+    vote_count = models.IntegerField(default=0)
+    like_count = models.IntegerField(default=0)
     # Maybe add an image field here.
