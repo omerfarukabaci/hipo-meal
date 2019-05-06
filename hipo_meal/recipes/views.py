0b46ec95317db4c36854e876018ceda26d927a89
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-05-07 02:32:16 +0300

    Connect view with database

--- hipo_meal/recipes/views.py
+++ hipo_meal/recipes/views.py
@@ -1,30 +1,8 @@
 from django.shortcuts import render
-
-dummy_recipe_data = [
-    {
-        'title': 'Musakka',
-        'image': '',
-        'content': 'Please deal with your meal kind.',
-        'post_date': '06/05/2019',
-        'vote_point': '3.5/5',
-        'vote_count': 13,
-        'like_count': 5,
-        'author': 'Ömer Faruk Abacı'
-    },
-    {
-        'title': 'Pan Seared Salmon',
-        'image': '',
-        'content': 'Please deal with your meal kind.',
-        'post_date': '06/05/2019',
-        'vote_point': '5/5',
-        'vote_count': 2,
-        'like_count': 0,
-        'author': 'Ömer Faruk Abacı 2'
-    }
-]
+from .models import Recipe
 
 def home(request):
     context = {
-        'recipes': dummy_recipe_data
+        'recipes': Recipe.objects.all()
     }
     return render(request, 'recipes/home.html', context)
