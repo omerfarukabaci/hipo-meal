881cd0a16a4a421e0c0f8fa691ef2ca4501c11bf
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-05-06 23:54:16 +0300

    Add home.html template and show some dummy data

--- hipo_meal/recipes/views.py
+++ hipo_meal/recipes/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+dummy_recipe_data = [
+    {
+        'title': 'Musakka',
+        'image': '',
+        'content': 'Please deal with your meal kind.',
+        'post_date': '06/05/2019',
+        'vote_point': '3.5/5',
+        'vote_count': 13,
+        'like_count': 5,
+        'author': 'Ömer Faruk Abacı'
+    },
+    {
+        'title': 'Pan Seared Salmon',
+        'image': '',
+        'content': 'Please deal with your meal kind.',
+        'post_date': '06/05/2019',
+        'vote_point': '5/5',
+        'vote_count': 2,
+        'like_count': 0,
+        'author': 'Ömer Faruk Abacı 2'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Home</h1>')
+    context = {
+        'recipes': dummy_recipe_data
+    }
+    return render(request, 'recipes/home.html', context)
