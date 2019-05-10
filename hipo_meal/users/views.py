8d609f30ccf097d5f327f376dc2af1ace3bd35d4
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-05-10 22:44:00 +0300

    Complete user registration page

--- hipo_meal/users/views.py
+++ hipo_meal/users/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 def register(request):
-    registration_form = UserCreationForm()
+    if request.method == "POST":
+        registration_form = UserRegisterForm(request.POST)
+        if registration_form.is_valid():
+            registration_form.save()
+            username = registration_form.cleaned_data.get('username')
+            messages.success(request, f'Welcome to our world {username}!')
+            return redirect('recipes-home')
+
+    else:
+        registration_form = UserRegisterForm()
+        
     context = {
         'registration_form': registration_form
     }
