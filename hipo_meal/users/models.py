3f9ec29c1c51781d48584bd7c5424a8e68b5f83a
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-05-11 02:26:42 +0300

    Override save method, resize profile images

--- hipo_meal/users/models.py
+++ hipo_meal/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image, ImageOps
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -7,3 +8,9 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+        image = Image.open(self.image.path)
+        image = ImageOps.fit(image, (150, 150), Image.ANTIALIAS)
+        image.save(self.image.path)
