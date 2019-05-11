4780fa1508d3e0ee5f290de59d7b18d57eff99e9
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-05-11 05:40:38 +0300

    Add recipe update view and separate templates of update and create

--- hipo_meal/recipes/views.py
+++ hipo_meal/recipes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Recipe
 
 class RecipeListView(ListView):
@@ -12,9 +13,19 @@ class RecipeListView(ListView):
 class RecipeDetailView(DetailView):
     model = Recipe
 
-class RecipeCreateView(CreateView):
+class RecipeCreateView(LoginRequiredMixin, CreateView):
     model = Recipe
     fields = ['title', 'content']
+    template_name = 'recipes/recipe_create_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class RecipeUpdateView(LoginRequiredMixin, UpdateView):
+    model = Recipe
+    fields = ['title', 'content']
+    template_name = 'recipes/recipe_update_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
