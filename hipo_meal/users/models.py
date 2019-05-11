6282cc7b4eca6639d48b3c5a748231557d1118c8
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-05-11 05:15:21 +0300

    Reorder class methods

--- hipo_meal/users/models.py
+++ hipo_meal/users/models.py
@@ -6,11 +6,11 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default_user_male.png', upload_to='profile_pics')
 
-    def __str__(self):
-        return f'{self.user.username} Profile'
-
     def save(self):
         super().save()
         image = Image.open(self.image.path)
         image = ImageOps.fit(image, (150, 150), Image.ANTIALIAS)
         image.save(self.image.path)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
