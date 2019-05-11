d209dd6e2506623397ff9d05299f3ea91b4115c6
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-05-11 22:58:00 +0300

    Add evaluation URL

--- hipo_meal/recipes/urls.py
+++ hipo_meal/recipes/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('recipe/new/', RecipeCreateView.as_view(), name='recipe-create'),
     path('recipe/<int:pk>/update/', RecipeUpdateView.as_view(), name='recipe-update'),
     path('recipe/<int:pk>/delete/', RecipeDeleteView.as_view(), name='recipe-delete'),
+    path('recipe/<int:pk>/evaluate/', views.evaluate_recipe, name='recipe-evaluate'),
 ]
