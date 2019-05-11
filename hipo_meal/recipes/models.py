cf3c04912e0e4b7efbc4088a63095d1a6af4c02e
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-05-11 07:06:19 +0300

    Fix adding recipe image

--- hipo_meal/recipes/models.py
+++ hipo_meal/recipes/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from PIL import Image, ImageOps
 
 class Recipe(models.Model):
     title = models.CharField(max_length=75)
@@ -11,10 +12,16 @@ class Recipe(models.Model):
     vote_points = models.IntegerField(default=0)
     vote_count = models.IntegerField(default=0)
     like_count = models.IntegerField(default=0)
-    image = models.ImageField(default='default_recipe.png', upload_to='profile_pics')
+    image = models.ImageField(default='default_recipe.png', upload_to='recipe_images')
 
     def get_absolute_url(self):
         return reverse('recipe-detail', kwargs={'pk': self.pk})
+
+    def save(self):
+        super().save()
+        image = Image.open(self.image.path)
+        image = ImageOps.fit(image, (500, 300), Image.ANTIALIAS)
+        image.save(self.image.path)
 
     def __str__(self):
         return self.title
