c79aaf05d3fdbfe9810ff22a37aedb04a0e5ce92
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-05-12 21:28:08 +0300

    Register ingredient model

--- hipo_meal/recipes/admin.py
+++ hipo_meal/recipes/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Recipe, Evaluation
+from .models import Recipe, Evaluation, Ingredient
 
 admin.site.register(Recipe)
 admin.site.register(Evaluation)
+admin.site.register(Ingredient)
