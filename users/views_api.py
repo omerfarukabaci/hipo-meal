07604e8fddc5c5948099a1d026717a88415ecb66
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-11 19:00:24 +0400

    Add login view

--- users/views_api.py
+++ users/views_api.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics, views
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
 from . import serializers
 from . import permissions
 
@@ -15,3 +17,23 @@ class UserRetrieveView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserListSerializer
     permission_classes = (permissions.IsAuthenticatedAndOwner,)
+
+class LoginView(views.APIView):
+    serializer_class = serializers.LoginSerializer
+
+    def post(self, request, **kwargs):
+        try:
+            user = User.objects.get(username=request.data["username"])
+        except:
+            return views.Response(data="User is not found.", status=404)
+        
+        if not user.check_password(request.data["password"]):
+            return views.Response(data="Authentication failed.", status=400)
+
+        token = Token.objects.get(user=user)
+        data = {
+            'token': token.key,
+            'username': user.username
+        }
+
+        return views.Response(data=data, status=200)
