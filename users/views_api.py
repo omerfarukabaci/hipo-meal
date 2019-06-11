562e341f1268fabebe22ad4d91f673bca9921b0d
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-11 15:29:04 +0400

    Create user retrieve and update view

--- users/views_api.py
+++ users/views_api.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
 from . import serializers
+from . import permissions
 
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -8,4 +9,15 @@ class UserListCreateView(generics.ListCreateAPIView):
     def get_serializer_class(self):
         if self.request.method == 'GET':
             return serializers.UserListSerializer
-        return serializers.UserCreateSerializer   
+        return serializers.UserCreateSerializer
+
+class UserRetrieveView(generics.RetrieveUpdateAPIView):
+    
+    queryset = User.objects.all()
+    serializer_class = serializers.UserListSerializer
+    permission_classes = (permissions.IsAuthenticatedAndOwner,)
+    
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return serializers.UserListSerializer
+        return serializers.UserCreateSerializer
