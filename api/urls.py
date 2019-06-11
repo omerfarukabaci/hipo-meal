ac6e86921d0a13288a54623739003919a655b8f3
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-11 12:33:30 +0400

    Implement user creation and user listing endpoints

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import include, path
 from . import views as api_views
 
 urlpatterns = [
-    path('users/', api_views.UserListView.as_view()),
+    path('users/', api_views.UserListCreateView.as_view()),
     path('auth/', include('rest_auth.urls')),
 ]
--- api/views.py
+++ api/views.py
@@ -2,6 +2,10 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from . import serializers
 
-class UserListView(generics.ListCreateAPIView):
+class UserListCreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
-    serializer_class = serializers.UserSerializer
+
+    def get_serializer_class(self):
+        if self.request.method == 'GET':
+            return serializers.UserListSerializer
+        return serializers.UserCreateSerializer   
