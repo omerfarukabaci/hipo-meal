460720766eb3f10a5780d77349f6c59c8c56f3e4
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-11 15:34:13 +0400

    Change retrieve view

--- users/views_api.py
+++ users/views_api.py
@@ -12,12 +12,6 @@ class UserListCreateView(generics.ListCreateAPIView):
         return serializers.UserCreateSerializer
 
 class UserRetrieveView(generics.RetrieveUpdateAPIView):
-    
     queryset = User.objects.all()
     serializer_class = serializers.UserListSerializer
     permission_classes = (permissions.IsAuthenticatedAndOwner,)
-    
-    def get_serializer_class(self):
-        if self.request.method == 'GET':
-            return serializers.UserListSerializer
-        return serializers.UserCreateSerializer
