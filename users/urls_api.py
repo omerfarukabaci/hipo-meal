60cfa64147e76e788e06b72988ad99472e4ad34a
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-11 19:00:05 +0400

    Add login endpoint

--- users/urls_api.py
+++ users/urls_api.py
@@ -4,4 +4,5 @@ from . import views_api
 urlpatterns = [
     path('users/', views_api.UserListCreateView.as_view()),
     path('users/<int:pk>/', views_api.UserRetrieveView.as_view()),
+    path('auth/login/', views_api.LoginView.as_view()),
 ]
