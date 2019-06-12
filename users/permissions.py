db6334dbcf46506717af23333fa9723496da4a6e
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-12 13:20:02 +0400

    Remove safe methods

--- users/permissions.py
+++ users/permissions.py
@@ -1,13 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 
-SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
-
-
 class IsOwner(IsAuthenticated):
     message = 'Permission denied.'
 
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        else:
-            return obj == request.user
+        return obj == request.user
