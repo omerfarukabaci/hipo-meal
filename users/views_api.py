43db1593a5e5acabab5f583eb2c029f8fe319cc2
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-12 12:30:50 +0400

    Add change password view

--- users/views_api.py
+++ users/views_api.py
@@ -1,3 +1,4 @@
+import django.contrib.auth.password_validation as password_validation
 from django.contrib.auth.models import User
 from rest_framework import generics, views
 from rest_framework.authtoken.models import Token
@@ -39,3 +40,21 @@ class LoginView(views.APIView):
         }
 
         return views.Response(data=data, status=200)
+
+
+class ChangePasswordView(generics.GenericAPIView):
+    serializer_class = serializers.ChangePasswordSerializer
+    permission_classes = (permissions.IsAuthenticatedAndOwner,)
+
+    def post(self, request, **kwargs):
+        serializer = serializers.ChangePasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            try:
+                password_validation.validate_password(serializer.data["new_password"])
+            except Exception as e:
+                return views.Response(data=e, status=400)
+            if not request.user.check_password(serializer.data["old_password"]):
+                return views.Response(data="Old password is incorrect.", status=400)
+            serializer.save(request.user)
+            return views.Response(serializer.data, status=200)
+        return views.Response(serializer.errors, status=400)
