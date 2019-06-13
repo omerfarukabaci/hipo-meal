9e9a1f7a1f840d4c5bd5388b1675e4e3d0d83d9f
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-13 10:45:22 +0400

    Remove overrided post function

--- users/views_api.py
+++ users/views_api.py
@@ -8,13 +8,6 @@ class UsersView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
 
-    def post(self, request, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return views.Response(serializer.data, status=200)
-        return views.Response(serializer.errors, status=400)
-
 
 class UserDetailView(generics.RetrieveUpdateAPIView):
     queryset = User.objects.all()
