d393cdbb133fbf15ac0792ce5705ed192c044cd2
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-13 11:15:30 +0400

    Change endpoint url

--- users/urls_api.py
+++ users/urls_api.py
@@ -3,7 +3,7 @@ from . import views_api
 
 urlpatterns = [
     path('users/', views_api.UsersView.as_view()),
-    path('users/<int:pk>/', views_api.UserDetailView.as_view()),
+    path('users/me/', views_api.UserDetailView.as_view()),
     path('auth/login/', views_api.LoginView.as_view()),
     path('auth/change-password/', views_api.ChangePasswordView.as_view()),
 ]
