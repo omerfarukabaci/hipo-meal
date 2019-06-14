b341889dd1b68dc89ca49aaff423a9d4db5893b7
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-14 18:56:01 +0400

    Add change password tests

--- users/tests.py
+++ users/tests.py
@@ -64,3 +64,41 @@ class UsersViewTestCase(APITestCase):
                                                                       "password": "testuser2pass123",
                                                                       "email": "test2@test.com"})
         self.assertEqual(response.status_code, 400)
+
+
+class ChangePasswordViewTestCase(APITestCase):
+    def setUp(self):
+        self.username = "test_user"
+        self.password = "testuserpass123"
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.token, created = Token.objects.get_or_create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_change_password_with_wrong_old_password(self):
+        response = self.client.post(reverse("api-change-password"), data={"old_password": "wrongpassword",
+                                                                          "new_password": "newpassword123"})
+        self.assertEqual(response.status_code, 400)
+        self.assertTrue("wrong" in response.content.decode('utf-8').lower())
+
+    def test_change_password_with_short_password(self):
+        response = self.client.post(reverse("api-change-password"), data={"old_password": "testuserpass123",
+                                                                          "new_password": "1234"})
+        self.assertEqual(response.status_code, 400)
+        self.assertTrue("short" in response.content.decode('utf-8').lower())
+
+    def test_change_password_with_common_password(self):
+        response = self.client.post(reverse("api-change-password"), data={"old_password": "testuserpass123",
+                                                                          "new_password": "1234abcd"})
+        self.assertEqual(response.status_code, 400)
+        self.assertTrue("common" in response.content.decode('utf-8').lower())
+
+    def test_change_password_with_numeric_password(self):
+        response = self.client.post(reverse("api-change-password"), data={"old_password": "testuserpass123",
+                                                                          "new_password": "06967464"})
+        self.assertEqual(response.status_code, 400)
+        self.assertTrue("numeric" in response.content.decode('utf-8').lower())
+
+    def test_change_password_successfully(self):
+        response = self.client.post(reverse("api-change-password"), data={"old_password": "testuserpass123",
+                                                                          "new_password": "newpassword123"})
+        self.assertEqual(response.status_code, 200)
