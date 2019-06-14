31c864617b530e359cdac85380819e27250d11fa
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-14 18:40:19 +0400

    Fix mistyped view names

--- users/tests.py
+++ users/tests.py
@@ -35,32 +35,32 @@ class UsersViewTestCase(APITestCase):
     def setUp(self):
         self.username = "test_user"
         self.password = "testuserpass123"
-        self.user = User.objects.create_user(self.username, self.email, self.password)
+        self.user = User.objects.create_user(username=self.username, password=self.password)
         self.token, created = Token.objects.get_or_create(user=self.user)
 
     def test_register_without_username(self):
-        response = self.client.post(reverse("api-login"), data={"password": "testuser2pass123",
-                                                                "email": "test2@test.com"})
+        response = self.client.post(reverse("api-create-user"), data={"password": "testuser2pass123",
+                                                                      "email": "test2@test.com"})
         self.assertEqual(response.status_code, 400)
 
     def test_register_without_password(self):
-        response = self.client.post(reverse("api-login"), data={"username": "test_user2",
-                                                                "email": "test2@test.com"})
+        response = self.client.post(reverse("api-create-user"), data={"username": "test_user2",
+                                                                      "email": "test2@test.com"})
         self.assertEqual(response.status_code, 400)
 
     def test_register_without_email(self):
-        response = self.client.post(reverse("api-login"), data={"username": "test_user2",
-                                                                "password": "testuser2pass123"})
-        self.assertEqual(response.status_code, 200)
+        response = self.client.post(reverse("api-create-user"), data={"username": "test_user2",
+                                                                      "password": "testuser2pass123"})
+        self.assertEqual(response.status_code, 201)
 
     def test_register_with_email(self):
-        response = self.client.post(reverse("api-login"), data={"username": "test_user2",
-                                                                "password": "testuser2pass123",
-                                                                "email": "test2@test.com"})
-        self.assertEqual(response.status_code, 200)
+        response = self.client.post(reverse("api-create-user"), data={"username": "test_user2",
+                                                                      "password": "testuser2pass123",
+                                                                      "email": "test2@test.com"})
+        self.assertEqual(response.status_code, 201)
 
     def test_register_with_current_username(self):
-        response = self.client.post(reverse("api-login"), data={"username": "test_user",
-                                                                "password": "testuser2pass123",
-                                                                "email": "test2@test.com"})
+        response = self.client.post(reverse("api-create-user"), data={"username": "test_user",
+                                                                      "password": "testuser2pass123",
+                                                                      "email": "test2@test.com"})
         self.assertEqual(response.status_code, 400)
