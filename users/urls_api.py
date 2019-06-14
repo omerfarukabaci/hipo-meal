740bb5c90672a87e310d8791d8a81f2b8e5ab60f
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-14 18:13:31 +0400

    Give name for url paths

--- users/urls_api.py
+++ users/urls_api.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views_api
 
 urlpatterns = [
-    path('users/', views_api.UsersView.as_view()),
-    path('users/me/', views_api.UserDetailView.as_view()),
-    path('auth/login/', views_api.LoginView.as_view()),
-    path('auth/change-password/', views_api.ChangePasswordView.as_view()),
+    path('users/', views_api.UsersView.as_view(), name="api-create-user"),
+    path('users/me/', views_api.UserDetailView.as_view(), name="api-edit-user"),
+    path('auth/login/', views_api.LoginView.as_view(), name="api-login"),
+    path('auth/change-password/', views_api.ChangePasswordView.as_view(), name="api-change-password"),
 ]
