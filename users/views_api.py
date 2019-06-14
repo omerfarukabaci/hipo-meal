fc51080b9e5fd370563f07bdf0dbb0fd9e03da3b
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-14 12:27:01 +0400

    Edit UserDetailView permissions.

--- users/views_api.py
+++ users/views_api.py
@@ -9,6 +9,7 @@ class UsersView(generics.CreateAPIView):
 
 class UserDetailView(generics.RetrieveUpdateAPIView):
     serializer_class = serializers.UserDetailSerializer
+    permission_classes = (permissions.IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
