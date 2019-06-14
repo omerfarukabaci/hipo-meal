ab42507b8a420c82bccc2b1443ae7f96778b4167
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-14 11:35:39 +0400

    Add default permission class

--- hipo_meal/settings.py
+++ hipo_meal/settings.py
@@ -141,5 +141,8 @@ django_heroku.settings(locals())
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     )
 }
