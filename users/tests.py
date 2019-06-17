5616b7cd7dda1a1fec611b2c9c2f9331925fb9f0
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-17 14:29:31 +0400

    Add namespace for api, rename endpoints' view names

--- users/tests.py
+++ users/tests.py
@@ -12,21 +12,21 @@ class UserLoginViewTestCase(APITestCase):
         self.token, created = Token.objects.get_or_create(user=self.user)
 
     def test_login_without_username(self):
-        response = self.client.post(reverse("api-login"), data={"password": self.password})
+        response = self.client.post(reverse("api-users:login"), data={"password": self.password})
         self.assertEqual(response.status_code, 400)
 
     def test_login_without_password(self):
-        response = self.client.post(reverse("api-login"), data={"username": self.username})
+        response = self.client.post(reverse("api-users:login"), data={"username": self.username})
         self.assertEqual(response.status_code, 400)
 
     def test_login_with_wrong_password(self):
-        response = self.client.post(reverse("api-login"), data={"username": self.username,
-                                                                "password": "wrongpassword"})
+        response = self.client.post(reverse("api-users:login"), data={"username": self.username,
+                                                                      "password": "wrongpassword"})
         self.assertEqual(response.status_code, 400)
 
     def test_login_successfully(self):
-        response = self.client.post(reverse("api-login"), data={"username": self.username,
-                                                                "password": self.password})
+        response = self.client.post(reverse("api-users:login"), data={"username": self.username,
+                                                                      "password": self.password})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data['token'], self.token.key)
 
@@ -39,28 +39,28 @@ class UsersViewTestCase(APITestCase):
         self.token, created = Token.objects.get_or_create(user=self.user)
 
     def test_register_without_username(self):
-        response = self.client.post(reverse("api-create-user"), data={"password": "testuser2pass123",
+        response = self.client.post(reverse("api-users:users"), data={"password": "testuser2pass123",
                                                                       "email": "test2@test.com"})
         self.assertEqual(response.status_code, 400)
 
     def test_register_without_password(self):
-        response = self.client.post(reverse("api-create-user"), data={"username": "test_user2",
+        response = self.client.post(reverse("api-users:users"), data={"username": "test_user2",
                                                                       "email": "test2@test.com"})
         self.assertEqual(response.status_code, 400)
 
     def test_register_without_email(self):
-        response = self.client.post(reverse("api-create-user"), data={"username": "test_user2",
+        response = self.client.post(reverse("api-users:users"), data={"username": "test_user2",
                                                                       "password": "testuser2pass123"})
         self.assertEqual(response.status_code, 201)
 
     def test_register_with_email(self):
-        response = self.client.post(reverse("api-create-user"), data={"username": "test_user2",
+        response = self.client.post(reverse("api-users:users"), data={"username": "test_user2",
                                                                       "password": "testuser2pass123",
                                                                       "email": "test2@test.com"})
         self.assertEqual(response.status_code, 201)
 
     def test_register_with_current_username(self):
-        response = self.client.post(reverse("api-create-user"), data={"username": "test_user",
+        response = self.client.post(reverse("api-users:users"), data={"username": "test_user",
                                                                       "password": "testuser2pass123",
                                                                       "email": "test2@test.com"})
         self.assertEqual(response.status_code, 400)
@@ -75,30 +75,30 @@ class ChangePasswordViewTestCase(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
     def test_change_password_with_wrong_old_password(self):
-        response = self.client.post(reverse("api-change-password"), data={"old_password": "wrongpassword",
-                                                                          "new_password": "newpassword123"})
+        response = self.client.post(reverse("api-users:change-password"), data={"old_password": "wrongpassword",
+                                                                                "new_password": "newpassword123"})
         self.assertEqual(response.status_code, 400)
         self.assertTrue("wrong" in response.content.decode('utf-8').lower())
 
     def test_change_password_with_short_password(self):
-        response = self.client.post(reverse("api-change-password"), data={"old_password": "testuserpass123",
-                                                                          "new_password": "1234"})
+        response = self.client.post(reverse("api-users:change-password"), data={"old_password": "testuserpass123",
+                                                                                "new_password": "1234"})
         self.assertEqual(response.status_code, 400)
         self.assertTrue("short" in response.content.decode('utf-8').lower())
 
     def test_change_password_with_common_password(self):
-        response = self.client.post(reverse("api-change-password"), data={"old_password": "testuserpass123",
-                                                                          "new_password": "1234abcd"})
+        response = self.client.post(reverse("api-users:change-password"), data={"old_password": "testuserpass123",
+                                                                                "new_password": "1234abcd"})
         self.assertEqual(response.status_code, 400)
         self.assertTrue("common" in response.content.decode('utf-8').lower())
 
     def test_change_password_with_numeric_password(self):
-        response = self.client.post(reverse("api-change-password"), data={"old_password": "testuserpass123",
-                                                                          "new_password": "06967464"})
+        response = self.client.post(reverse("api-users:change-password"), data={"old_password": "testuserpass123",
+                                                                                "new_password": "06967464"})
         self.assertEqual(response.status_code, 400)
         self.assertTrue("numeric" in response.content.decode('utf-8').lower())
 
     def test_change_password_successfully(self):
-        response = self.client.post(reverse("api-change-password"), data={"old_password": "testuserpass123",
-                                                                          "new_password": "newpassword123"})
+        response = self.client.post(reverse("api-users:change-password"), data={"old_password": "testuserpass123",
+                                                                                "new_password": "newpassword123"})
         self.assertEqual(response.status_code, 200)
--- users/urls_api.py
+++ users/urls_api.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views_api
 
+app_name = 'api-users'
+
 urlpatterns = [
-    path('users/', views_api.UsersView.as_view(), name="api-create-user"),
-    path('users/me/', views_api.UserDetailView.as_view(), name="api-edit-user"),
-    path('auth/login/', views_api.LoginView.as_view(), name="api-login"),
-    path('auth/change-password/', views_api.ChangePasswordView.as_view(), name="api-change-password"),
+    path('users/', views_api.UsersView.as_view(), name="users"),
+    path('users/me/', views_api.MyUserDetailView.as_view(), name="my-user-detail"),
+    path('auth/login/', views_api.LoginView.as_view(), name="login"),
+    path('auth/change-password/', views_api.ChangePasswordView.as_view(), name="change-password"),
 ]
