5105e7fabd85e7c024114ed1c7cf3866f7d0ec66
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-18 17:18:23 +0400

    Add tag serializer
    Add custom choice field
    
    Edit RecipeSerializer's fields

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,15 +1,31 @@
-from .models import Recipe, Ingredient, User
+from .models import Recipe, Ingredient
 from rest_framework import serializers
-from pytz import timezone
+
+
+class ChoiceField(serializers.ChoiceField):
+    def to_representation(self, obj):
+        return self._choices[obj]
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('name',)
+        extra_kwargs = {
+            'name': {'validators': []},
+        }
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    ingredients = serializers.ListField(child=serializers.CharField())
-    difficulty = serializers.IntegerField(min_value=1, max_value=4)
+    ingredients = IngredientSerializer(many=True)
+    difficulty = ChoiceField(choices=Recipe.DIFFICULTY_CHOICES)
+    author = serializers.SerializerMethodField(source='get_author')
+    date_posted = serializers.DateTimeField(required=False)
 
     class Meta:
         model = Recipe
-        fields = ('title', 'content', 'vote_points', 'vote_count', 'like_count', 'difficulty', 'ingredients')
+        fields = ('title', 'content', 'difficulty', 'ingredients',
+                  'vote_points', 'vote_count', 'like_count', 'author', 'date_posted')
 
     def create(self, data):
         request = self.context.get("request")
@@ -17,28 +33,15 @@ class RecipeSerializer(serializers.ModelSerializer):
         recipe = Recipe(title=data["title"], content=data["content"],
                         author=author)
         recipe.save()
-        for ingredient_name in data["ingredients"]:
-            lookup_name = ingredient_name.lower()
+        for ingredient in data["ingredients"]:
+            lookup_name = ingredient["name"].lower()
             lookup_name = lookup_name.replace(" ", "_")
-            ingredient, created = Ingredient.objects.get_or_create(name=ingredient_name, lookup_name=lookup_name)
+            ingredient, created = Ingredient.objects.get_or_create(name=ingredient["name"], lookup_name=lookup_name)
             recipe.ingredients.add(ingredient)
 
         recipe.difficulty = str(data["difficulty"])
         recipe.save()
         return recipe
 
-    def to_representation(self, data):
-        ingredient_names = []
-        for ingredient in data.ingredients.all():
-            ingredient_names.append(ingredient.name)
-
-        return {
-            'title': data.title,
-            'content': data.content,
-            'difficulty': data.get_difficulty_display(),
-            'ingredients': ingredient_names,
-            'likes': data.like_count,
-            'votes': data.vote_points / data.vote_count if data.vote_count else 0,
-            'author': User.objects.get(id=data.author_id).username,
-            'date_posted': data.date_posted.astimezone(timezone('Europe/Istanbul')).strftime("%Y-%m-%d %H:%M")
-        }
+    def get_author(self, obj):
+        return obj.author.username
