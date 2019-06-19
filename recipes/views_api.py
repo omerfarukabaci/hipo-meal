926b6938883caf588ec602a21cf511d6f34d0155
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-19 17:52:18 +0400

    Override get_queryset methods of recipe views Override destroy function

--- recipes/views_api.py
+++ recipes/views_api.py
@@ -1,16 +1,26 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, views
 from . import serializers
 from core import permissions as custom_permissions
 from .models import Recipe
 
 
 class RecipesView(generics.ListCreateAPIView):
-    queryset = Recipe.objects.all()
     serializer_class = serializers.RecipeSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
+    def get_queryset(self):
+        return Recipe.objects.filter(is_hidden=False)
+
 
 class RecipesDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Recipe.objects.all()
     serializer_class = serializers.RecipeSerializer
     permission_classes = (custom_permissions.IsOwnerOrReadOnly,)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.is_hidden = True
+        instance.save()
+        return views.Response(status=204)
+
+    def get_queryset(self):
+        return Recipe.objects.filter(is_hidden=False)
