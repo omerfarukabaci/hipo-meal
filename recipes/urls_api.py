a9c5993b03020641391d8e115183575fb70535c7
Ömer Faruk Abacı <abaci15@itu.edu.tr>
2019-06-19 11:13:47 +0400

    Add endpoint for recipe detail view

--- recipes/urls_api.py
+++ recipes/urls_api.py
@@ -5,4 +5,5 @@ app_name = 'api-recipes'
 
 urlpatterns = [
     path('recipes/', views_api.RecipesView.as_view(), name="recipes"),
+    path('recipes/<int:pk>/', views_api.RecipesDetailView.as_view(), name="recipes-detail")
 ]
